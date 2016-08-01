3f2a0a92ac48e45a07bf455d1a954c96b24c714e
Ted Chen <ted.chen@emc.com>
2016-08-01 13:46:48 +0000

    modify wait-completion-uri to make use of task notification

--- spec/lib/jobs/wait-completion-uri-spec.js
+++ spec/lib/jobs/wait-completion-uri-spec.js
@@ -8,6 +8,7 @@ describe(require('path').basename(__filename), function () {
         WaitCompletionJob;
     var subscribeRequestPropertiesStub;
     var subscribeHttpResponseStub;
+    var subscribeTaskNotification;
 
     before(function() { 
         helper.setupInjector([
@@ -21,6 +22,10 @@ describe(require('path').basename(__filename), function () {
             WaitCompletionJob.prototype, '_subscribeHttpResponse', function(cb) {
                 cb({statusCode: 200, url: 'completion'});
         });
+        subscribeTaskNotification = sinon.stub(
+            WaitCompletionJob.prototype, '_subscribeTaskNotification', function(cb) {
+                cb({statusCode: 200, url: 'completion'});
+        });
     });
 
     it("should run", function() {
@@ -28,6 +33,7 @@ describe(require('path').basename(__filename), function () {
         job._run().then(function() {
             expect(subscribeRequestPropertiesStub).to.have.been.called;
             expect(subscribeHttpResponseStub).to.have.been.called;
+            expect(subscribeTaskNotification).to.have.been.called;
         });
     });
 });
